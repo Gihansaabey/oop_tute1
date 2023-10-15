b49ceaae4d0b69d8f0fd4f650217001ef2e55c5c
Gihansaabey <abeygunawardenagihansa@gmail.com>
2023-10-15 16:05:24 +0530

    initial commit

---
+++ src/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String nam){
+        name=nam;
+    }
+    public void displayName(){
+        System.out.println(name);
+    }
+
+    public void  setSurname(String sName){
+        surname=sName;
+
+
+    }
+    public void setAge(int mAge){
+        age=mAge;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+    public int getAge(){
+        return age;
+    }
+}
